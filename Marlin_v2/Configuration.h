ad9a6ed9633829a11d3238292374116f9436e117
ccccmagicboy <ccccmagicboy@users.noreply.github.com>
2020-05-25 13:31:32 +0000

    [cron] update the marlin v2 firmware: 20200525_213131_CST+0800

--- Marlin_v2/Configuration.h
+++ Marlin_v2/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 97.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 100, 97.5 }
 
 /**
  * Default Max Feed Rate (mm/s)

b9b032ab345cc7a945532df0a4b16ccf2574cd79
ccccmagicboy <ccccmagicboy@users.noreply.github.com>
2020-05-27 06:40:16 +0000

    [cron] update the marlin v2 firmware for cr10mini: 20200527_144015_CST+0800

--- Marlin_v2/cr10mini/Configuration.h
+++ Marlin_v2/cr10mini/Configuration.h
@@ -1109,7 +1109,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 300
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1117,7 +1117,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 300
+#define Z_MAX_POS 290
 
 /**
  * Software Endstops

880609db80f058fa0c8407e3a2a371ee23581b89
ccccmagicboy <ccccmagicboy@users.noreply.github.com>
2020-05-27 06:23:11 +0000

    [cron] update the marlin v2 firmware for cr10mini: 20200527_142310_CST+0800

--- Marlin_v2/cr10mini/Configuration_adv.h
+++ Marlin_v2/cr10mini/Configuration_adv.h
@@ -2378,7 +2378,7 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  #define HYBRID_THRESHOLD
+  //#define HYBRID_THRESHOLD
 
   #define X_HYBRID_THRESHOLD 130
   #define X2_HYBRID_THRESHOLD    100
